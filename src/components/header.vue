a8450063ac1133565bac41ec9d63aa5c4ad1c6f2
manho30 <manhochan30@gmail.com>
2023-02-25 21:44:57 +0800

    fix error

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="bg-white border-gray-200 px-4 mb-3 lg:px-6 py-2.5">
-    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl py-2 md:py-4">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
       <official/>
-      <div class="hidden justify-center items-center w-full md:flex w-auto order-1">
-        <ul class="flex md:flex-row lg:space-x-8">
+      <div class="hidden justify-center items-center w-full lg:flex lg:w-auto lg:order-1">
+        <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
           <li>
             <a href="/" class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0" aria-current="page">Home</a>
           </li>
